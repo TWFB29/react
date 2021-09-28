40529e752cca3721457539b11275dac2a4d8d06b
twfb <twfb29@yahoo.com>
2021-09-27 20:15:14 -0400

    20.1

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,24 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <h1 id="nav">Taylor</h1>
+    </section>
+
   );
 }
 
